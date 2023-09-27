cc1fef0729d3fadf518c8cc288658152d0417101
mythriojas <mythriojas06@gmail.com>
2023-09-27 11:04:10 +0530

    react-auth commit1

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { Container, Col, Row } from "react-bootstrap";
+import "./App.css";
+import Register from "./Register";
+
+function App() {
+  return (
+    <Container>
+      <Row>
+        <Col xs={12} sm={12} md={6} lg={6}>
+          <Register />
+        </Col>
+
+        <Col xs={12} sm={12} md={6} lg={6}></Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default App;
